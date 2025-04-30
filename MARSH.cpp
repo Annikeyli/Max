45be00d148b3c56e27ca3a10e06a46c7e3cc3543
Annikeyli <anna20061010@yandex.ru>
2025-04-30 12:47:25 +0300

    Add files via upload

---
+++ MARSH.cpp
@@ -0,0 +1,170 @@
+﻿	//#include "MARSH.h" 
+	//#include <iostream> 
+	//
+	//MARSH::MARSH() {}
+	//
+	//MARSH::MARSH(const char* n, const char* s, int z) {
+	//    strcpy_s(nazv_nach, s);
+	//    strcpy_s(nazv_kon, n);
+	//    number_marsh = z;
+	//}
+	//
+	//MARSH::MARSH(MARSH& m) {
+	//    strcpy_s(nazv_nach, m.nazv_nach);
+	//    strcpy_s(nazv_kon, m.nazv_kon);
+	//    number_marsh = m.number_marsh;
+	//}
+	//
+	//bool MARSH::operator<(MARSH& z)
+	//{
+	//    return this->number_marsh < z.number_marsh;
+	//}
+	//
+	//bool MARSH::compMARSH(MARSH& z, MARSH& z1)
+	//{
+	//    return z.number_marsh < z1.number_marsh;
+	//}
+	//
+	//
+	//
+	//ostream& operator<<(ostream& os, const MARSH& m) {
+	//    setlocale(LC_ALL, "Russian");
+	//    return os << "Маршрут №" << m.number_marsh << ": "
+	//        << m.nazv_nach << " - " << m.nazv_kon;
+	//}
+	//
+	//istream& operator>>(istream& is, MARSH& m) {
+	//    is >> m.nazv_nach >> m.nazv_kon >> m.number_marsh;
+	//    return is;
+	//}
+	//
+//#include "MARSH.h"
+//#include <iostream>
+//#include <clocale>
+//
+//// Конструктор по умолчанию
+//MARSH::MARSH() : number_marsh(0) {
+//	nazv_nach[0] = '\0';
+//	nazv_kon[0] = '\0';
+//}
+//
+//// Параметризованный конструктор
+//MARSH::MARSH(const char* start, const char* end, int num) : number_marsh(num) {
+//	strncpy_s(nazv_nach, start, sizeof(nazv_nach) - 1);
+//	strncpy_s(nazv_kon, end, sizeof(nazv_kon) - 1);
+//	nazv_nach[sizeof(nazv_nach) - 1] = '\0';
+//	nazv_kon[sizeof(nazv_kon) - 1] = '\0';
+//}
+//
+//// Конструктор копирования
+//MARSH::MARSH(const MARSH& other) : number_marsh(other.number_marsh) {
+//	strncpy_s(nazv_nach, other.nazv_nach, sizeof(nazv_nach));
+//	strncpy_s(nazv_kon, other.nazv_kon, sizeof(nazv_kon));
+//}
+//
+//// Оператор сравнения для сортировки
+//bool MARSH::operator<(const MARSH& other) const {
+//	return number_marsh < other.number_marsh;
+//}
+//
+//// Статический метод сравнения
+//bool MARSH::compMARSH(const MARSH& a, const MARSH& b) {
+//	return a.number_marsh < b.number_marsh;
+//}
+//
+//// Вывод в поток (с поддержкой русского языка)
+//std::ostream& operator<<(std::ostream& os, const MARSH& m) {
+//	setlocale(LC_ALL, "Russian");  // Для корректного отображения кириллицы
+//	os << "Маршрут №" << m.number_marsh << ": "
+//		<< m.nazv_nach << " → " << m.nazv_kon;
+//	return os;
+//}
+//
+//// Ввод из потока
+//std::istream& operator>>(std::istream& is, MARSH& m) {
+//	is >> m.nazv_nach >> m.nazv_kon >> m.number_marsh;
+//	return is;
+//}
+//#include "MARSH.h"
+//#include <clocale>
+//
+//MARSH::MARSH() : number_marsh(0) {
+//	nazv_nach[0] = '\0';
+//	nazv_kon[0] = '\0';
+//}
+//
+//MARSH::MARSH(const char* start, const char* end, int num) : number_marsh(num) {
+//	strncpy(nazv_nach, start, 19);
+//	strncpy(nazv_kon, end, 19);
+//	nazv_nach[19] = '\0';
+//	nazv_kon[19] = '\0';
+//}
+//
+//MARSH::MARSH(const MARSH& other) : number_marsh(other.number_marsh) {
+//	strncpy(nazv_nach, other.nazv_nach, 19);
+//	strncpy(nazv_kon, other.nazv_kon, 19);
+//	nazv_nach[19] = '\0';
+//	nazv_kon[19] = '\0';
+//}
+//
+//bool MARSH::operator<(const MARSH& other) const {
+//	return number_marsh < other.number_marsh;
+//}
+//
+//bool MARSH::compMARSH(const MARSH& a, const MARSH& b) {
+//	return a.number_marsh < b.number_marsh;
+//}
+//
+//std::ostream& operator<<(std::ostream& os, const MARSH& m) {
+//	setlocale(LC_ALL, "Russian");
+//	os << "Маршрут №" << m.number_marsh << ": "
+//		<< m.nazv_nach << " → " << m.nazv_kon;
+//	return os;
+//}
+//
+//std::istream& operator>>(std::istream& is, MARSH& m) {
+//	is >> m.nazv_nach >> m.nazv_kon >> m.number_marsh;
+//	return is;
+//}
+
+#include "MARSH.h"
+//#include <clocale>
+
+MARSH::MARSH() : number_marsh(0) {
+	nazv_nach[0] = '\0';
+	nazv_kon[0] = '\0';
+}
+
+MARSH::MARSH(const char* start, const char* end, int num) : number_marsh(num) {
+	strncpy(nazv_nach, start, 19);
+	strncpy(nazv_kon, end, 19);
+	nazv_nach[19] = '\0';
+	nazv_kon[19] = '\0';
+}
+
+MARSH::MARSH(const MARSH& other) : number_marsh(other.number_marsh) {
+	strncpy(nazv_nach, other.nazv_nach, 19);
+	strncpy(nazv_kon, other.nazv_kon, 19);
+	nazv_nach[19] = '\0';
+	nazv_kon[19] = '\0';
+}
+
+bool MARSH::operator<(const MARSH& other) const {
+	return number_marsh < other.number_marsh;
+}
+
+bool MARSH::compMARSH(const MARSH& a, const MARSH& b) {
+	return a.number_marsh < b.number_marsh;
+}
+
+std::ostream& operator<<(std::ostream& os, const MARSH& m) {
+	setlocale(LC_ALL, "Russian");
+	os << "Маршрут №" << m.number_marsh << ": "
+		<< m.nazv_nach << " → " << m.nazv_kon;
+	return os;
+}
+
+std::istream& operator>>(std::istream& is, MARSH& m) {
+	is >> m.nazv_nach >> m.nazv_kon >> m.number_marsh;
+	return is;
+}
---
+++ Maksim_Epkh_3_zadanie.cpp
@@ -0,0 +1,302 @@
+﻿//#include <fstream>
+//#include <ios>
+//#include <string.h>
+//#include "MARSH.h"
+//#include <iostream>
+//#include "MY_ARRAY.h"
+//#include "SORT.h"
+//
+//
+//using namespace std;
+//void printArr(int* a, int n) {
+//    for (int i = 0; i < n; i++)
+//        cout << a[i] << " ";
+//}
+//
+//bool compInt(int& a, int& b) {
+//    return a < b;
+//}
+//
+//int main()
+//{
+//    My_Array<MARSH> arr;
+//    ifstream in("MARSH.txt.txt");
+//    if (!in)cout << "\nfile not open\n";
+//    in >> arr;
+//    cout << arr;
+//
+//    arr.sort(MARSH::compMARSH);
+//
+//    cout << "\nAfter sort\n" << arr;
+//    ofstream outbin("a.bin", ios_base::binary);
+//    arr.writeBin(outbin);
+//    ifstream inbin("a.bin", ios_base::binary);
+//
+//    int number;
+//    cout << "\nEnter number: ";
+//    cin >> number;
+//    bool found = false;
+//    for (int i = 0; i < 8; i++) {
+//        if (arr[i].number_marsh == number) {
+//            cout << arr[i] << "\n";
+//            found = true;
+//        }
+//    }
+//    if (!found) {
+//        cout << "No murshrut.\n";
+//    }
+//    return 0;
+//}
+
+//#include <fstream>
+//#include <ios>
+//#include <string.h>
+//#include "MARSH.h"
+//#include <iostream>
+//#include "MY_ARRAY.h"
+//#include "SORT.h"
+//
+//using namespace std;
+//
+//void printArr(int* a, int n) {
+//    for (int i = 0; i < n; i++)
+//        cout << a[i] << " ";
+//}
+//
+//bool compInt(int& a, int& b) {
+//    return a < b;
+//}
+//
+//int main()
+//{
+//    setlocale(LC_ALL, "Russian");
+//    My_Array<MARSH> arr;
+//    ifstream in("MARSH.txt");
+//    if (!in) {
+//        cout << "\nFile not open\n";
+//        return 1;
+//    }
+//    in >> arr;
+//    cout << arr;
+//
+//    arr.sort(MARSH::compMARSH);
+//    cout << "\nПосле сортировки\n" << arr;
+//
+//    ofstream outbin("a.bin", ios_base::binary);
+//    if (!outbin) {
+//        cout << "\nFailed to create binary file\n";
+//        return 1;
+//    }
+//    arr.writeBin(outbin);
+//    outbin.close();
+//
+//    ifstream inbin("a.bin", ios_base::binary);
+//    if (!inbin) {
+//        cout << "\nFailed to open binary file\n";
+//        return 1;
+//    }
+//
+//    int number;
+//    cout << "\nВведите номер маршрута:   ";
+//    cin >> number;
+//
+//    bool found = false;
+//    for (int i = 0; i < 8; i++) {  // Используем arr.size() вместо фиксированного значения 8
+//        if (arr[i].number_marsh == number) {
+//            cout << arr[i] << "\n";
+//            found = true;
+//        }
+//    }
+//
+//    if (!found) {
+//        cout << "Маршрут не найден  \n";
+//    }
+//
+//    return 0;
+//}
+//#include <fstream>
+//#include <iostream>
+//#include "MARSH.h"
+//#include "MY_ARRAY.h"
+//#include "SORT.h"
+//
+//void printArr(int* a, int n) {
+//    for (int i = 0; i < n; i++)
+//        std::cout << a[i] << " ";
+//}
+//
+//bool compInt(int& a, int& b) {
+//    return a < b;
+//}
+//
+//int main() {
+//    setlocale(LC_ALL, "Russian");
+//
+//    My_Array<MARSH> arr;
+//    std::ifstream in("MARSH.txt");
+//    if (!in) {
+//        std::cerr << "Ошибка открытия файла MARSH.txt\n";
+//        return 1;
+//    }
+//
+//    try {
+//        in >> arr;
+//        std::cout << "Исходные данные:\n" << arr;
+//
+//        // Сортировка через ваш класс SORT
+//        SORT<MARSH>::sort(arr, MARSH::compMARSH);
+//        std::cout << "\nПосле сортировки:\n" << arr;
+//
+//        // Запись в бинарный файл
+//        std::ofstream outbin("a.bin", std::ios::binary);
+//        if (!outbin) {
+//            std::cerr << "Ошибка создания бинарного файла\n";
+//            return 1;
+//        }
+//        arr.writeBin(outbin);
+//        outbin.close();
+//
+//        // Поиск маршрута
+//        int number;
+//        std::cout << "\nВведите номер маршрута: ";
+//        std::cin >> number;
+//
+//        bool found = false;
+//        for (size_t i = 0; i < arr.size(); i++) {
+//            if (arr[i].getNumber() == number) {
+//                std::cout << "Найден маршрут: " << arr[i] << "\n";
+//                found = true;
+//            }
+//        }
+//
+//        if (!found) {
+//            std::cout << "Маршрут с номером " << number << " не найден.\n";
+//        }
+//
+//    }
+//    catch (const std::exception& e) {
+//        std::cerr << "Ошибка: " << e.what() << "\n";
+//        return 1;
+//    }
+//
+//    return 0;
+//}
+// 
+//#include <fstream>
+//#include <iostream>
+//#include "MARSH.h"
+//#include "MY_ARRAY.h"
+//#include "SORTCOMP.h"
+//
+//int main() {
+//    setlocale(LC_ALL, "Russian");
+//
+//    My_Array<MARSH> arr;
+//    std::ifstream in("MARSH.txt");
+//    if (!in) {
+//        std::cerr << "Ошибка открытия файла MARSH.txt\n";
+//        return 1;
+//    }
+//
+//    try {
+//        in >> arr;
+//        std::cout << "Исходные данные:\n" << arr;
+//
+//        // Сортировка
+//        /*arr.sort(MARSH::compMARSH);
+//        std::cout << "\nПосле сортировки:\n" << arr;*/
+//
+//        // Запись в бинарный файл
+//        std::ofstream outbin("a.bin", std::ios::binary);
+//        if (!outbin) {
+//            std::cout << "Ошибка создания бинарного файла\n";
+//            return 1;
+//        }
+//        arr.writeBin(outbin);
+//        outbin.close();
+//
+//        // Поиск маршрута
+//        int number;
+//        std::cout << "\nВведите номер маршрута: ";
+//        std::cin >> number;
+//
+//        bool found = false;
+//        for (int i = 0; i < arr.size(); i++) {
+//            if (arr[i].getNumber() == number) {
+//                std::cout << "Найден маршрут: " << arr[i] << "\n";
+//                found = true;
+//            }
+//        }
+//
+//        if (!found) {
+//            std::cout << "Маршрут с номером " << number << " не найден.\n";
+//        }
+//
+//    }
+//    catch (const std::exception& e) {
+//        std::cout << "Ошибка: " << "\n";
+//        return 1;
+//    }
+//
+//    return 0;
+//}
+#include <fstream>
+#include <iostream>
+#include "MARSH.h"
+#include "MY_ARRAY.h"
+#include "SORTCOMP.h"
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    My_Array<MARSH> arr;
+    std::ifstream in("MARSH.txt");
+    if (!in) {
+        std::cerr << "Ошибка открытия файла MARSH.txt\n";
+        return 1;
+    }
+
+    // Чтение данных
+    in >> arr;
+    if (arr.size() == 0) {
+        std::cerr << "Ошибка: не удалось прочитать данные или файл пуст\n";
+        return 1;
+    }
+
+    std::cout << "Исходные данные:\n" << arr;
+
+    //// Сортировка
+    //arr.sort(MARSH::compMARSH);
+    //std::cout << "\nПосле сортировки:\n" << arr;
+
+    // Запись в бинарный файл
+    std::ofstream outbin("a.bin", std::ios::binary);
+    if (!outbin) {
+        std::cerr << "Ошибка создания бинарного файла\n";
+        return 1;
+    }
+    arr.writeBin(outbin);
+    outbin.close();
+
+    // Поиск маршрута
+    int number;
+    std::cout << "\nВведите номер маршрута: ";
+    if (!(std::cin >> number)) {
+        std::cerr << "Ошибка ввода номера маршрута\n";
+        return 1;
+    }
+
+    bool found = false;
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr[i].getNumber() == number) {
+            std::cout << "Найден маршрут: " << arr[i] << "\n";
+            found = true;
+        }
+    }
+
+    if (!found) {
+        std::cout << "Маршрут с номером " << number << " не найден.\n";
+    }
+
+    return 0;
+}
